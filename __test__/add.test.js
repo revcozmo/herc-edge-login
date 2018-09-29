0e8edd457655b1bbfa68f47cc75a365cd5057c71
julianapeace <chancecordelia@gmail.com>
2018-09-29 18:26:11 -0500

    added test

--- __test__/add.test.js
+++ __test__/add.test.js
@@ -1,4 +1,9 @@
+/* globals test expect */
+
+// This is an example test file
+
 const add = require('./add');
+
 describe('add', () => {
   it('should add two numbers', () => {
     expect(add(1, 2)).toBe(3);
