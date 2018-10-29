05d0e3f6ca2435e42ceb958994ce91faa9a99bc4
julianapeace <chancecordelia@gmail.com>
2018-10-29 04:55:09 -0500

    removed welcome screen from main navigation

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -45,7 +45,6 @@ import QRCapture from "../screens/QRCapture";
 import QRCapture2 from "../screens/QRCapture2";
 import styles from "../assets/styles";
 import DrawerIcon from "../assets/icons/drawerIcon.png";
-import Welcome from "../screens/Welcome";
 
 
 
@@ -123,7 +122,6 @@ const MainNavigator = StackNavigator({ //this will be "createStackNavigator" aft
     Wallet: { screen: Wallet },
     Settings: { screen: Settings },
     Camera: { screen: Camera },
-    Welcome: {screen: Welcome}
 
 }, {
         initialRouteName: 'Login',
