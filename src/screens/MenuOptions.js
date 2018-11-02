324b6f8d9b7f9a49f3d198e3d7acfe8cb5fa59eb
julianapeace <chancecordelia@gmail.com>
2018-11-02 17:05:29 -0500

    updated version # to 0.9.1

--- src/screens/MenuOptions.js
+++ src/screens/MenuOptions.js
@@ -81,7 +81,7 @@ class MenuOptions extends Component {
                     </TouchableHighlight> */}
 
                     <Text style={{ color: "#f3c736", alignSelf: "flex-end", fontSize: 8 }}>
-                        V.0.2.9
+                        V.0.9.1
                     </Text>
                 </View>
             </View>
