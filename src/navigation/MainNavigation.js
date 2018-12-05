3a1181192a7b98708dac179a4079e3170dd73478
julianapeace <chancecordelia@gmail.com>
2018-12-05 01:45:26 -0600

    linked hiprTransactions page to main navigation

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -22,6 +22,7 @@ import DocUp from "../screens/DocUp";
 import EdiT from "../screens/Edi-T";
 import FileUp from "../screens/FileUp";
 import Hipr from "../screens/Hipr";
+import HiprTransactions from "../screens/HiprTransactions";
 import HiprAssets from "../screens/HiprAssets";
 import InputMan from "../screens/InputMan";
 import Login from "../screens/Login";
